3586d088077d9444b864e98c0eb48f50371e0ac9
Landar-S <landarsergij@gmail.com>
2022-10-08 22:43:46 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let intervalId = null;
 
+startBtn.addEventListener('click', onActiveChangeColor);
+stopBtn.addEventListener('click', onStopChangeColor);
+
+function onActiveChangeColor() {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+}
+
+function onStopChangeColor() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
