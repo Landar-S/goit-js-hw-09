ac4b2864ca544e48a03770565c2c10b7748a6cc0
Landar-S <landarsergij@gmail.com>
2022-10-11 22:33:32 +0300

    Task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,51 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onSubmitForm);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  promise
+    .then(({ position, delay }) => {
+      setTimeout(
+        () =>
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          ),
+        delay
+      );
+    })
+    .catch(({ position, delay }) => {
+      setTimeout(
+        () =>
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          ),
+        delay
+      );
+    });
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  let position = 1;
+  let newDelay = Number(formEl.elements.delay.value);
+
+  for (let i = 0; i < formEl.elements.amount.value; i += 1) {
+    createPromise(position, newDelay);
+    position += 1;
+    newDelay += Number(formEl.elements.step.value);
   }
 }
